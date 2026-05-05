4ec5af1fad28a6e7f48709ea50a76fd3e5d0e3f3
MatheusFVieira <matheus.vf06@gmail.com>
2026-05-04 22:34:06 -0300

    Aula_11

---
+++ delivery-api/projeto-api/src/main/java/com/delivery/api/ClienteController.java
@@ -0,0 +1,34 @@
+package com.delivery.api;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ClienteController {
+
+    private List<Cliente> clientes = new ArrayList<>();
+    private Long proximoId = 1L;
+
+    @GetMapping("/clientes")
+    public List<Cliente> ListaTodos(){
+        return clientes;
+    }
+
+    @PostMapping("/clientes")
+    public Cliente cadastrar(@RequestBody Cliente cliente){
+        cliente.setId(proximoId);
+        proximoId++;
+        clientes.add(cliente);
+        return  cliente;
+    }
+
+
+
+}
+
+
+
+
+
+ 
